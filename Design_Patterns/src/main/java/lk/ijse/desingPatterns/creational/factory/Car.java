9e7d0654f453262e2f7d3c67a7d453137657bd5f
Gathsara2002 <liviru2002mmg@gmail.com>
2023-11-22 17:41:31 +0530

    Added car class

---
+++ Design_Patterns/src/main/java/lk/ijse/desingPatterns/creational/factory/Car.java
@@ -0,0 +1,15 @@
+package lk.ijse.desingPatterns.creational.factory;
+
+/**
+ * @author : Gathsara
+ * created : 11/22/2023 -- 5:40 PM
+ **/
+
+/*Sub class / Concrete product*/
+
+public class Car implements Vehicle{
+    @Override
+    public void drive() {
+
+    }
+}
--- Design_Patterns/src/main/java/lk/ijse/desingPatterns/creational/factory/Vehicle.java
+++ Design_Patterns/src/main/java/lk/ijse/desingPatterns/creational/factory/Vehicle.java
@@ -4,6 +4,11 @@ package lk.ijse.desingPatterns.creational.factory;
  * @author : Gathsara
  * created : 11/22/2023 -- 5:37 PM
  **/
+
+/*This interface is super for all sub classes.
+This is use to get common return type in factory method.
+If you want to add any common methods that use in sub classes, add to this interface*/
+
 public interface Vehicle {
     void drive();
 }
