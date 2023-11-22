00df9640b271e8fbc2dde5aa5e6bc986ba97857a
Gathsara2002 <liviru2002mmg@gmail.com>
2023-11-22 17:41:57 +0530

    Added bike class

---
+++ Design_Patterns/src/main/java/lk/ijse/desingPatterns/creational/factory/Bike.java
@@ -0,0 +1,12 @@
+package lk.ijse.desingPatterns.creational.factory;
+
+/**
+ * @author : Gathsara
+ * created : 11/22/2023 -- 5:41 PM
+ **/
+public class Bike implements Vehicle{
+    @Override
+    public void drive() {
+
+    }
+}
