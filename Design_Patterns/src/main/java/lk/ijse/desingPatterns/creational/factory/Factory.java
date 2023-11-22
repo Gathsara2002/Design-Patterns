0c675c8a638e02a1c638b1d85efa94854383a83b
Gathsara2002 <liviru2002mmg@gmail.com>
2023-11-22 17:52:58 +0530

    Factory design pattern is done

--- Design_Patterns/src/main/java/lk/ijse/desingPatterns/creational/factory/Factory.java
+++ Design_Patterns/src/main/java/lk/ijse/desingPatterns/creational/factory/Factory.java
@@ -4,5 +4,16 @@ package lk.ijse.desingPatterns.creational.factory;
  * @author : Gathsara
  * created : 11/21/2023 -- 9:11 PM
  **/
+
 public class Factory {
+    public static void main(String[] args) {
+        VehicleFactory vehicleFactory = new VehicleFactoryImpl();
+
+        Vehicle car = vehicleFactory.createVehicle("Car");
+        car.drive();
+
+        Vehicle bike = vehicleFactory.createVehicle("Bike");
+        bike.drive();
+
+    }
 }
