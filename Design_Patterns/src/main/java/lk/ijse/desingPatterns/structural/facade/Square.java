6260a69613a8b6c523f57fd2b0038020b7a357e9
Gathsara2002 <liviru2002mmg@gmail.com>
2023-11-24 18:48:20 +0530

    Added square sub system class

---
+++ Design_Patterns/src/main/java/lk/ijse/desingPatterns/structural/facade/Square.java
@@ -0,0 +1,13 @@
+package lk.ijse.desingPatterns.structural.facade;
+
+/**
+ * @author : Gathsara
+ * created : 11/24/2023 -- 6:47 PM
+ **/
+
+public class Square implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Draw a square");
+    }
+}
