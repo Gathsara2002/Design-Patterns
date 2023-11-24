685602edec4faf714ba21bcbf7fbe73fde61ae4f
Gathsara2002 <liviru2002mmg@gmail.com>
2023-11-24 18:46:06 +0530

    Added shape inteface

---
+++ Design_Patterns/src/main/java/lk/ijse/desingPatterns/structural/facade/Shape.java
@@ -0,0 +1,11 @@
+package lk.ijse.desingPatterns.structural.facade;
+
+/**
+ * @author : Gathsara
+ * created : 11/24/2023 -- 6:44 PM
+ **/
+
+/*sub system interface*/
+public interface Shape {
+    void draw();
+}
