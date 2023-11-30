48ca7ebbc820074aed247593ab1921fb88a5a8eb
Gathsara2002 <liviru2002mmg@gmail.com>
2023-11-30 20:06:30 +0530

    Added Test class

---
+++ Design_Patterns/src/main/java/lk/ijse/desingPatterns/behavioural/strategy/Test.java
@@ -0,0 +1,11 @@
+package lk.ijse.desingPatterns.behavioural.strategy;
+
+/**
+ * @author : Gathsara
+ * created : 11/30/2023 -- 8:05 PM
+ **/
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
