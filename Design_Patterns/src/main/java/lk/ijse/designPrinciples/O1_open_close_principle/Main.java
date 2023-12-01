22a3d9fb8a8c4fc4742808314b2bcc6401bd6043
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-01 22:06:27 +0530

    Added design principles

---
+++ Design_Patterns/src/main/java/lk/ijse/designPrinciples/O1_open_close_principle/Main.java
@@ -0,0 +1,11 @@
+package lk.ijse.designPrinciples.O1_open_close_principle;
+
+/**
+ * @author : Gathsara
+ * created : 12/1/2023 -- 10:04 PM
+ **/
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
