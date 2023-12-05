b30f38be6944010ea71488b6c71d8eedd93507a6
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-05 17:26:03 +0530

    Added new  class

---
+++ Design_Patterns/src/main/java/lk/ijse/designPrinciples/O3_open_close_principle/service/types/CurrentAccount.java
@@ -0,0 +1,21 @@
+package lk.ijse.designPrinciples.O3_open_close_principle.service.types;
+
+import lk.ijse.designPrinciples.O3_open_close_principle.service.Account;
+
+/**
+ * @author : Gathsara
+ * created : 12/5/2023 -- 5:25 PM
+ **/
+public class CurrentAccount extends Account {
+
+    @Override
+    public void deposit(double amount) {
+        System.out.println(amount + " Deposited to your Current Account.!");
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        System.out.println(amount + " Withdrawn from your Current Account.!");
+    }
+
+}
