0c7957dbbecb9e59005d90983c073295a528ebb9
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-05 17:26:46 +0530

    Added new  class

---
+++ Design_Patterns/src/main/java/lk/ijse/designPrinciples/O3_open_close_principle/service/types/SavingsAccount.java
@@ -0,0 +1,21 @@
+package lk.ijse.designPrinciples.O3_open_close_principle.service.types;
+
+import lk.ijse.designPrinciples.O3_open_close_principle.service.Account;
+
+/**
+ * @author : Gathsara
+ * created : 12/5/2023 -- 5:26 PM
+ **/
+public class SavingsAccount extends Account {
+
+    @Override
+    public void deposit(double amount) {
+        System.out.println(amount + " Deposited to your Savings Account.!");
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        System.out.println(amount + " Withdrawn from your Savings Account.!");
+    }
+
+}
