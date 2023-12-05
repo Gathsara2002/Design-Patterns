f483f3a087c57a293fde33f2a938cb719ce1f3e4
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-05 17:03:08 +0530

    Added new class

---
+++ Design_Patterns/src/main/java/lk/ijse/designPrinciples/O2_open_close_principle/BankAppWithdrawalService.java
@@ -0,0 +1,42 @@
+package lk.ijse.designPrinciples.O2_open_close_principle;
+
+/**
+ * @author : Gathsara
+ * created : 12/5/2023 -- 5:01 PM
+ **/
+public class BankAppWithdrawalService {
+
+    public void depositToSavingsAccount(double amount){
+        System.out.println(amount + " Deposited to your Savings Account.!");
+    }
+
+    public void depositToCurrentAccount(double amount){
+        System.out.println(amount + " Deposited to your Current Account.!");
+    }
+
+    public void withdrawFromSavingsAccount(double amount){
+        System.out.println(amount + " Withdrawn from your Savings Account.!");
+    }
+
+    public void withdrawFromCurrentAccount(double amount){
+        System.out.println(amount + " Withdrawn from your Current Account.!");
+    }
+
+    /* I want to implement another feature for Banking App Withdrawal Service*/
+
+    public void depositToSalarySaverAccount(double amount){
+        System.out.println(amount + " Deposited to your Salary Server Account.!");
+    }
+
+    public void withdrawFromSalarySaverAccount(double amount){
+        System.out.println(amount + " Withdrawn from your Salary Server Account.!");
+    }
+
+    /* So here for adding a new feature I had to modify the class */
+    /* So, unfortunately we have violated the Open / Close principle */
+    /* Why.? */
+    /* In Open/Close principle a class should be closed for modifications but opened for extensions */
+    /* But here we did modifications rather than doing extensions */
+    /* S0, we violate the rule */
+
+}
--- Design_Patterns/src/main/java/lk/ijse/designPrinciples/O2_open_close_principle/Main.java
+++ Design_Patterns/src/main/java/lk/ijse/designPrinciples/O2_open_close_principle/Main.java
@@ -5,4 +5,22 @@ package lk.ijse.designPrinciples.O2_open_close_principle;
  * created : 12/5/2023 -- 5:00 PM
  **/
 public class Main {
+
+    /*SOLID*/
+    /* O - Open / Close Principle */
+    /*
+     *This principle states that the software entities should be open for extensions but close
+     * for modifications
+     * */
+    public static void main(String[] args) {
+
+        BankAppWithdrawalService bankAppWithdrawalService = new BankAppWithdrawalService();
+
+        bankAppWithdrawalService.depositToCurrentAccount(100000);
+        bankAppWithdrawalService.withdrawFromCurrentAccount(5000);
+
+        bankAppWithdrawalService.depositToSalarySaverAccount(200000);
+        bankAppWithdrawalService.withdrawFromSalarySaverAccount(100000);
+    }
+
 }
