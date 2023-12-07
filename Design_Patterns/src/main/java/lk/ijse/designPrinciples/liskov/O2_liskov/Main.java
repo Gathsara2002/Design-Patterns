9d30fb8eafd9264f114b1be2fd561f607e7bf1b3
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-07 12:55:18 +0530

    Content updated

--- Design_Patterns/src/main/java/lk/ijse/designPrinciples/liskov/O2_liskov/Main.java
+++ Design_Patterns/src/main/java/lk/ijse/designPrinciples/liskov/O2_liskov/Main.java
@@ -5,6 +5,7 @@ import lk.ijse.designPrinciples.liskov.O1_liskov.service.Account;
 import lk.ijse.designPrinciples.liskov.O1_liskov.service.types.CurrentAccount;
 import lk.ijse.designPrinciples.liskov.O1_liskov.service.types.SalarySaverAccount;
 import lk.ijse.designPrinciples.liskov.O1_liskov.service.types.SavingAccount;
+import lk.ijse.designPrinciples.liskov.O2_liskov.service.types.FixedAccount;
 
 /**
  * @author : Gathsara
@@ -45,6 +46,25 @@ public class Main {
         lk.ijse.designPrinciples.liskov.O1_liskov.BankAppWithdrawalService bankAppWithdrawalService3 = new BankAppWithdrawalService(salaryServer);
         bankAppWithdrawalService3.depositToAccount(500000);
         bankAppWithdrawalService3.withdrawFromAccount(100000);
+
+        /* Let's focus on this case */
+        /*Super class*/           /*Sub Class */
+        Account fixedAccount = new FixedAccount(); // feels like ok
+        /* This is not ok, because it's breaking the application structure by doing this */
+
+        BankAppWithdrawalService bankAppWithdrawalService4 = new BankAppWithdrawalService(fixedAccount);
+        bankAppWithdrawalService4.depositToAccount(1500000); // This is totally fine
+        bankAppWithdrawalService4.withdrawFromAccount(100000);// But this one..? // fixed account should not support for this feature
+
+        /*
+         * In here there is an issue. though there is no compile error on the code, we have already violated
+         * the liskov-substitution principle
+         * How.?
+         * In liskov-Substitution principle it emphasizes that objects of a superclass shall be replaceable with
+         * Objects of its subclasses without breaking the application. In here by replacing FixedAccount for its
+         * Super Class Account, it's breaking the application structure, because fixed accounts don't own withdraw option
+         * So it's violating the Liskov-Substitution Principle
+         * */
 
     }
 }
