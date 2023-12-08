03efc400a395eb100c74d981aabb6c5716cb05ae
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-08 17:39:51 +0530

    Added new interfaces

---
+++ Design_Patterns/src/main/java/lk/ijse/designPrinciples/interface_segregation/O3_interface_segregation/service/custom/Loan.java
@@ -0,0 +1,8 @@
+package lk.ijse.designPrinciples.interface_segregation.O3_interface_segregation.service.custom;
+
+/**
+ * @author : Gathsara
+ * created : 12/8/2023 -- 5:39 PM
+ **/
+public interface Loan {
+}
