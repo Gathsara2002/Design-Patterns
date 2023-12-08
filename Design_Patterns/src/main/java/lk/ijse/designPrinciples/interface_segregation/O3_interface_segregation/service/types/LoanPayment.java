d30f55711e061b80c7bfba012639458a2faabef4
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-08 17:39:11 +0530

    Added new class

---
+++ Design_Patterns/src/main/java/lk/ijse/designPrinciples/interface_segregation/O3_interface_segregation/service/types/LoanPayment.java
@@ -0,0 +1,8 @@
+package lk.ijse.designPrinciples.interface_segregation.O3_interface_segregation.service.types;
+
+/**
+ * @author : Gathsara
+ * created : 12/8/2023 -- 5:39 PM
+ **/
+public class LoanPayment {
+}
