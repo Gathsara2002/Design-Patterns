6d632fbcecaf84d64884dd280295956dd26ae888
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-08 17:38:05 +0530

    Added new interface

---
+++ Design_Patterns/src/main/java/lk/ijse/designPrinciples/interface_segregation/O3_interface_segregation/service/Payment.java
@@ -0,0 +1,8 @@
+package lk.ijse.designPrinciples.interface_segregation.O3_interface_segregation.service;
+
+/**
+ * @author : Gathsara
+ * created : 12/8/2023 -- 5:37 PM
+ **/
+public interface Payment {
+}
