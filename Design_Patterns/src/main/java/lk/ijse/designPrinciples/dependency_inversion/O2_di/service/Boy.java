7b1260fe7a220090e9751835cba7616968eba68e
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-08 18:41:46 +0530

    Dependency inversion principle is checked

--- Design_Patterns/src/main/java/lk/ijse/designPrinciples/dependency_inversion/O2_di/service/Boy.java
+++ Design_Patterns/src/main/java/lk/ijse/designPrinciples/dependency_inversion/O2_di/service/Boy.java
@@ -4,5 +4,23 @@ package lk.ijse.designPrinciples.dependency_inversion.O2_di.service;
  * @author : Gathsara
  * created : 12/8/2023 -- 6:35 PM
  **/
+
 public class Boy {
+    SuperGirl girl= new Girl(); // property injection
+
+    public void chatWithGirl(){
+        //Girl girl = new Girl(); // Violate DI Principle
+        // Now both depend on abstraction
+        girl.chat();
+    }
+    /*The classes which are performing tasks with the help of other classes are known as High Level Classes
+     * Boy class is using the help of Girl class to perform chat with girl task
+     * So boy is a High Level Class.
+     *
+     * So what Dependency Inversion Principle Emphasize on.?
+     * High level modules should not depend upon low level modules. So here we are violating it.
+     *
+     * So, then what we should do.?
+     * Both should depend upon abstraction
+     * */
 }
--- Design_Patterns/src/main/java/lk/ijse/designPrinciples/dependency_inversion/O2_di/service/Girl.java
+++ Design_Patterns/src/main/java/lk/ijse/designPrinciples/dependency_inversion/O2_di/service/Girl.java
@@ -4,5 +4,10 @@ package lk.ijse.designPrinciples.dependency_inversion.O2_di.service;
  * @author : Gathsara
  * created : 12/8/2023 -- 6:35 PM
  **/
-public class Girl {
+public class Girl extends SuperGirl {
+    /* This is a low level class, because this class functions are used by another class called Boy*/
+    @Override
+    public void chat() {
+        System.out.println("Hello there...D");
+    }
 }
