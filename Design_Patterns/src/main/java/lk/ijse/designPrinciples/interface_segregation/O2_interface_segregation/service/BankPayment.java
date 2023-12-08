ee35027b39f892835fd8a789c9061cbad190399e
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-08 17:25:27 +0530

    Added new class

---
+++ Design_Patterns/src/main/java/lk/ijse/designPrinciples/interface_segregation/O2_interface_segregation/service/BankPayment.java
@@ -0,0 +1,8 @@
+package lk.ijse.designPrinciples.interface_segregation.O2_interface_segregation.service;
+
+/**
+ * @author : Gathsara
+ * created : 12/8/2023 -- 5:25 PM
+ **/
+public class BankPayment {
+}
