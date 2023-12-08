1be135cd2e5b933581f17b011dd81fea1aeb3848
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-08 17:22:39 +0530

    Content updated

--- Design_Patterns/src/main/java/lk/ijse/designPrinciples/interface_segregation/O1_interface_segregation/service/Payment.java
+++ Design_Patterns/src/main/java/lk/ijse/designPrinciples/interface_segregation/O1_interface_segregation/service/Payment.java
@@ -1,8 +1,16 @@
 package lk.ijse.designPrinciples.interface_segregation.O1_interface_segregation.service;
 
+import java.util.List;
+
 /**
  * @author : Gathsara
  * created : 12/8/2023 -- 5:19 PM
  **/
+
 public interface Payment {
+    void initiatePayments();
+
+    Object status();
+
+    List<Object> getPayments();
 }
--- Design_Patterns/src/main/java/lk/ijse/designPrinciples/interface_segregation/O1_interface_segregation/service/types/BankPayment.java
+++ Design_Patterns/src/main/java/lk/ijse/designPrinciples/interface_segregation/O1_interface_segregation/service/types/BankPayment.java
@@ -1,8 +1,32 @@
 package lk.ijse.designPrinciples.interface_segregation.O1_interface_segregation.service.types;
 
+import lk.ijse.designPrinciples.interface_segregation.O1_interface_segregation.service.Payment;
+
+import java.util.List;
+
 /**
  * @author : Gathsara
  * created : 12/8/2023 -- 5:20 PM
  **/
-public class BankPayment {
+public class BankPayment implements Payment {
+
+    @Override
+    public void initiatePayments() {
+        // Payment Initiate Logic
+        System.out.println("Payment Initiated");
+    }
+
+    @Override
+    public Object status() {
+        // Status returning logic
+        System.out.println("Status Called");
+        return null;
+    }
+
+    @Override
+    public List<Object> getPayments() {
+        // Get all payments
+        System.out.println("Returned all payments");
+        return null;
+    }
 }
