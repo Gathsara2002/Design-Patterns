022a11235558daebf68df47f1fc74341d729b43e
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-13 12:33:36 +0530

    Added new classes

---
+++ Design_Patterns/src/main/java/lk/ijse/desingPatterns/structural/decorator/Square.java
@@ -0,0 +1,12 @@
+package lk.ijse.desingPatterns.structural.decorator;
+
+/**
+ * @author : Gathsara
+ * created : 12/13/2023 -- 12:32 PM
+ **/
+public class Square implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Draw a square");
+    }
+}
