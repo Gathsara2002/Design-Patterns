6cccc10f6f7a5e0d5081c48558506501ab38acd0
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-13 11:03:16 +0530

    Added new class

---
+++ Design_Patterns/src/main/java/lk/ijse/desingPatterns/behavioural/observer/service/impl/Observer2.java
@@ -0,0 +1,14 @@
+package lk.ijse.desingPatterns.behavioural.observer.service.impl;
+
+import lk.ijse.desingPatterns.behavioural.observer.service.Observer;
+
+/**
+ * @author : Gathsara
+ * created : 12/13/2023 -- 11:02 AM
+ **/
+public class Observer2 implements Observer {
+    @Override
+    public void update(String msg) {
+        System.out.println("Hi , Im observer 2");
+    }
+}
